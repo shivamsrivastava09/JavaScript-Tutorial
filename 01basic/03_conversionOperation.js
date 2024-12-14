2ba154e6b4ad3dd3168f7d83b3b94b06639c9e56
Shivam Srivastava <shivam0174srivastava@gmail.com>
2024-12-14 18:25:09 +0530

    done with the conversion topic

--- 01basic/03_conversionOperation.js
+++ 01basic/03_conversionOperation.js
@@ -22,4 +22,25 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof(stringNumber));
 
+// *********************Operations*******************//
+
+let value = 3
+let negValue = -value
+console.log(negValue );
+
+let str1 ="hello"
+let str2 = " shivam"
+let str3 = str1+str2
+console.log(str3);
+
+console.log(1+"3");
+console.log("1"+3+5);
+console.log(1+3+"5");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
 
