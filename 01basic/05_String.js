1e4adec9068183f93fca35ad1a08eac5e6b3db07
Shivam Srivastava <shivam0174srivastava@gmail.com>
2024-12-16 20:32:33 +0530

    learned some of the String Concepts

---
+++ 01basic/05_String.js
@@ -0,0 +1,10 @@
+const name = "Shivam"
+const repoCount = 10
+
+console.log(`hello my name is ${name} and my repository count is ${repoCount}`);
+
+const gameName = new String ('Shivam Srivastava')
+
+console.log(gameName.length);
+
+// refer to mdn for documentation
