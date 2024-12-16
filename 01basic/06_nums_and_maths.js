5eff60105288be43ae2e472adef81394353f12a8
Shivam Srivastava <shivam0174srivastava@gmail.com>
2024-12-16 21:48:45 +0530

    done with the numbers and maths in JavaScript

---
+++ 01basic/06_nums_and_maths.js
@@ -0,0 +1,20 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(500);
+console.log(balance);
+
+
+// ++++++++++++++ MATH +++++++++++++++
+
+console.log(Math.abs(-4));
+// ceil() floor() round(), min(), max(), random()
+
+console.log(Math.random());
+
+// dice game
+
+const min =1
+const max =6
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
