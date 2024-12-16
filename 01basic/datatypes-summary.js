e4af749a38fe93b8e3d5a683603cb9519212cfb4
Shivam Srivastava <shivam0174srivastava@gmail.com>
2024-12-16 20:11:44 +0530

    Call by value and call by reference

--- 01basic/datatypes-summary.js
+++ 01basic/datatypes-summary.js
@@ -11,3 +11,32 @@ const id = Symbol('123')
 const anotherId = Symbol('123')
 
 console.log(id == anotherId); // false symbol is for defining something uniquely.
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/*
+    Types of Memory
+    1. Stack Memory (Primitive)
+    2. Heap Memory  (Non-Primitive)
+
+*/
+
+let myYoutube = "hiteshchoudhary"
+let anotherName = myYoutube
+console.log(anotherName);
+anotherName = "chaiaurcode"
+console.log(anotherName);
+console.log(myYoutube);
+
+let userOne = {
+    email:"abc@google.com",
+    upi:"shv@ibl"
+}
+
+let userTwo = userOne
+userTwo.email = "def@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
