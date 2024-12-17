287d3ea257b2af738b005d0bda7356f0831301d5
Shivam Srivastava <shivam0174srivastava@gmail.com>
2024-12-17 20:04:32 +0530

    Done with the Date and Time in Js

---
+++ 01basic/07_dates_and_time.js
@@ -0,0 +1,20 @@
+// Dates
+
+let myDates = new Date()
+console.log(myDates.toString());
+console.log(myDates.toDateString());
+console.log(myDates.toISOString());
+console.log(myDates.toLocaleString());
+console.log(typeof myDates);
+
+let myCreatedDate = new Date(2023, 2, 23)
+console.log(myCreatedDate.toDateString());
+
+let newDate = new Date("2023-01-14")
+console.log(newDate.toLocaleString());
+
+
+
+
+
+
