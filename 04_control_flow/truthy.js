aca1698b71d9d9b4a86b56c1f680a0bc3c384cc4
Shivam Srivastava <shivam0174srivastava@gmail.com>
2024-12-24 00:58:19 +0530

    Iterations with for loop

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -11,5 +11,14 @@ else{
     false, 0, -0, 0n(in BigInt), "", null, undefined, NaN
 
     truthy value :
+    "0", 'false', " ", [], {}, function(){}
 
 */
+
+// Nullish Coalescing Operator (??) : null undefined
+let user
+user = null ?? 10 // instead of 10 actually we write a complex function that calls the database and returns something or if it doesn't return something further code can be adjusted for null.
+console.log(user) // 10
+
+// Terniary Operator
+// condition ? true : false
---
+++ 05_iterations/one.js
@@ -0,0 +1,20 @@
+// for loop
+for (let i = 0; i < 10; i++) {
+    const element = i
+    //console.log(element);
+}
+
+// Nested loop :
+
+for (let i = 0; i < 10; i++) {
+    //console.log(`outer loop ${i}`);
+    for(let j=0;j<10;j++){
+       //console.log(`inner loop ${j}`);
+    }
+    
+}
+
+// break and continue keyword works same as in java.
+
+
+
